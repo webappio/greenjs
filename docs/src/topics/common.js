c8eb1de76b9e26161052b16e26e053edb3536732
Colin Chartier <colin@webapp.io>
2021-12-17 12:09:37 -0600

    Add responsive drawer logic

--- docs/src/topics/common.js
+++ docs/src/topics/common.js
@@ -4,6 +4,7 @@ import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Slide from '@mui/material/Slide';
 import {Link} from "@greenio/router";
 import {Drawer} from "@mui/material";
+import { Spin as Hamburger } from 'hamburger-react'
 
 function HideOnScroll({children}) {
     const slideTrigger = useScrollTrigger({});
@@ -17,9 +18,15 @@ function HideOnScroll({children}) {
 
 const drawerWidth = 240;
 
+function Sidebar() {
+    return <div className="mt-20 mx-6 d-flex">
+        <h1>sidebar</h1>
+    </div>
+}
+
 function DocBase({children}) {
-    const [drawerOpen, setDrawerOpen] = React.useState(true);
-    return <div className="flex">
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+    return <div className="flex items-center">
         <HideOnScroll>
             <AppBar
                 color="inherit"
@@ -28,7 +35,18 @@ function DocBase({children}) {
                 elevation={0}
             >
                 <div className="container flex mx-6 my-3 justify-between">
-                    <Link href="/"><img src="/static/images/Logo.svg" alt="GreenJS logo" width="96" height="48"/></Link>
+                    <div className="flex">
+                        <div className="md:hidden mr-3">
+                            <Hamburger
+                                toggled={drawerOpen}
+                                toggle={x => setDrawerOpen(x)}
+                                label="expand sidebar"
+                                distance="sm"
+                                size={20}
+                            />
+                        </div>
+                        <Link href="/"><img src="/static/images/Logo.svg" alt="GreenJS logo" width="96" height="48" className="w-auto h-full"/></Link>
+                    </div>
                     <div className="flex text-lg font-bold uppercase">
                         <input type="text" className="border border-slate-200 rounded-md px-2 grow shrink" placeholder="Search..."/>
                         <a href="https://join.slack.com/t/greenjs/shared_invite/zt-109by8mrn-p9gbRlSovBXvoM_5VZn31g" className="ml-8 hidden md:flex hover:underline">
@@ -47,14 +65,17 @@ function DocBase({children}) {
                     boxSizing: 'border-box',
                 },
             }}
-            variant="persistent"
+            variant="temporary"
             anchor="left"
             open={drawerOpen}
+            className="md:hidden"
         >
-
+            <Sidebar />
         </Drawer>
-        <div className="flex flex-col">
-            <div className="p-6">&nbsp;</div>
+        <div className="hidden md:flex w-[240px]">
+            <Sidebar />
+        </div>
+        <div className="flex flex-col pt-28 px-4 sm:px-8 md:px-12 container">
             {children}
         </div>
     </div>
