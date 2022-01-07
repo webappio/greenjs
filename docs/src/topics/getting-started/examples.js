1798783e81d58a514f00f2144c0d62e516d668d5
Colin Chartier <colin@webapp.io>
2022-01-06 23:09:51 -0600

    Add tailwindcss config

--- docs/src/topics/getting-started/examples.js
+++ docs/src/topics/getting-started/examples.js
@@ -4,7 +4,7 @@ import {DocBase, DocLink} from "../base-doc-page";
 export default function Page() {
     return <DocBase>
         <h1 className="font-bold text-2xl mb-4">Examples</h1>
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-2">
             <DocLink href="https://github.com/webappio/greenjs/tree/master/examples/hello-world-react">
                 A very simple 'hello world' app
             </DocLink>
--- docs/src/topics/integrations/tailwindcss.js
+++ docs/src/topics/integrations/tailwindcss.js
@@ -1,11 +1,29 @@
 import React from "react";
-import {DocBase} from "../base-doc-page";
+import {Code, DocBase, DocCmd} from "../base-doc-page";
 
 export default function Page() {
     return <DocBase>
-        <h1 className="font-bold text-2xl mb-4">TailwindCSS</h1>
-        <p>To use TailwindCSS with greenjs, run <code>npx create-greenjs-app</code></p>
-        <p>This will create a folder named <i>greenjs-example</i> with the following directory structure:</p>
-
+        <h1 className="font-bold text-2xl mb-4">Using TailwindCSS with GreenJS</h1>
+        <p className="my-2">
+            1. Install three packages: <DocCmd>npm install --save-dev tailwindcss concurrently greenjs</DocCmd>
+        </p>
+        <p className="my-2">
+            2. Change <DocCmd>package.json</DocCmd> to include the following lines:
+        </p>
+        <Code>{`
+"scripts": {
+  "build": "tailwindcss -i App.css -o ./dist/Site.css --minify && greenjs build",
+  "start": "mkdir -p dist && concurrently 'greenjs start' 'tailwindcss -i App.css -o ./dist/Site.css --watch'"
+}
+        `}</Code>
+        <p className="my-2">
+            3. Reference your TailwindCSS file by adding it to <DocCmd>App.js</DocCmd>
+        </p>
+        <Code>{`
+<Head>
+    <link rel="stylesheet" href="/Site.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</Head>
+        `}</Code>
     </DocBase>
 }
