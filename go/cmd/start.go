e8486f7bea2141f6f2628450f0d085960ba5f765
Colin Chartier <colin@webapp.io>
2022-01-25 22:36:32 -0500

    Find a free port for dev server

--- go/cmd/start.go
+++ go/cmd/start.go
@@ -14,8 +14,8 @@ func Start(args []string) {
 	flags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 
 	var listenAddr string
-	flags.StringVar(&listenAddr, "l", "127.0.0.1:8000", "the address to listen on (e.g., 0.0.0.0:8000)")
-	flags.StringVar(&listenAddr, "listen-addr", "127.0.0.1:8000", "the address to listen on (e.g., 0.0.0.0:8000)")
+	flags.StringVar(&listenAddr, "l", "", "the address to listen on (e.g., 0.0.0.0:8000)")
+	flags.StringVar(&listenAddr, "listen-addr", "", "the address to listen on (e.g., 0.0.0.0:8000)")
 
 	var upstreamAddr string
 	flags.StringVar(&upstreamAddr, "u", "", "the upstream server to forward requests to (e.g., localhost:8080)")
@@ -32,9 +32,22 @@ func Start(args []string) {
 		log.Fatal(err)
 	}
 
-	listener, err := net.Listen("tcp", listenAddr)
-	if err != nil {
-		log.Fatal(err)
+	var listener net.Listener
+	if listenAddr == "" {
+		for i := 3000; i < 3010; i += 1 {
+			listener, err = net.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", i))
+			if err == nil {
+				break
+			}
+		}
+		if listener == nil {
+			log.Fatal("Could not find a free port to listen to")
+		}
+	} else {
+		listener, err = net.Listen("tcp", listenAddr)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	fmt.Println("Server is listening at http://"+listener.Addr().String())
 	err = (&devserver.GreenJsServer{
